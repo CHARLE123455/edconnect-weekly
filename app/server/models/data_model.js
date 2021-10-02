57d0106374f1efb9f41d87d6d4a6bd1778c8686f
CHARLE123455 <kelejoe79@gmail.com>
2021-10-02 01:31:22 +0100

    Added initial JavaScript files to the Project Explorer Project

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -9,9 +9,16 @@ class DataModel {
     }
 
     getById(id) {
+        let getId = this.data.find(ele => ele.id === id)
+   if(!getId){
+       return null;
+   }
+   else{
+       return getId;
+   }
 
-    }
-
+     
+   }
     save(obj) {
         if (this.validate(obj)) {
             this.data.push(obj);
@@ -21,10 +28,26 @@ class DataModel {
     }
 
     update(obj, id) {
-
+        let elementsIndex = this.data.find(element => element.id == id )
+        if(!elementsIndex){
+            return false;
+        }
+        for(let key in obj){
+            elementsIndex[key] = obj[key];
+        }
+        return true;
     }
 
-    delete(id) {
+    delete(id){
+        let user = this.data.find(item => item.id === id);
+
+    let index = this.data.indexOf(user);
+    if (user) {
+        this.data.splice(index, 1);
+        return true;
+    }
+    return false;
+
 
     }
 
--- app/server/models/users.js
+++ app/server/models/users.js
@@ -2,31 +2,54 @@ const DataModel = require('./data_model');
 
 class User {
     constructor(id, firstname, lastname, email, password, matricNumber, program, graduationYear) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.matricNumber = matricNumber;
+        this.program = program;
+        this.graduationYear = graduationYear;
 
     }
 
     getFullName() {
-
+        let userName = firstName +' ' + lastName;
+        return userName;
     }
 }
 
 class Users extends DataModel {
     authenticate(email, password) {
-
+    let validUser = this.data.find((element) => element.email === email && element.password === password)
+      return (validUser? true : false); 
     }
 
     getByEmail(email) {
-
+    let getEmail = this.data.find(ele => ele.email === email)
+    return getEmail? getEmail : null;
     }
 
     getByMatricNumber(matricNumber) {
-
+        let element = this.data.find(ele => ele.matricNumber === matricNumber)
+        return element? element : null;
     }
 
     validate(obj) {
-
+        let value = true;
+        for(let prop in obj ){
+            if(obj[prop] === null){
+                value = false;
+            }
+        }
+      let valEmail = this.data.find(ele => ele.email === obj.email)
+      let valMatric = this.data.find(ele => ele.matricNumber === obj.matricNumber)
+      let valPassword = obj.password.length >= 7;
+      return value && valPassword && !valEmail && !valMatric;
     }
 }
+
+
 
 // Do not worry about the below for now; It is included so that we can test your code
 // We will cover module exports in later parts of this course
