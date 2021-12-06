18c0cc8a55d2559ef995c5fe55e314f3a9752fbb
Charles Owuala <kelejoe79@gmail.com>
2021-12-06 11:47:36 +0100

    Completed Week 6 Exercise

---
+++ app/server/models/data_model.js
@@ -0,0 +1,73 @@
+class DataModel {
+    constructor() {
+        this.data = [];
+        this.errors = [];
+    }
+
+    getAll() {
+        return this.data;
+    }
+
+    getById(id) {
+        let result = this.data.find(obj => obj.id === id) 
+        if(!result){
+            return null;
+        }else{
+            return result;
+        }
+      
+    }
+
+    save(obj) {
+        if (this.validate(obj)) {
+            this.data.push(obj);
+            return true;
+        }
+        return false;
+    }
+    
+    update(obj, id) {
+        let found = false;
+        for (let user of this.data) {
+            for (let prop in user) {
+                if (user[prop] === id) {
+                    found = true;
+                    Object.assign(user,obj);
+                }
+            }
+        }
+        
+        if (found === true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    delete(id) {
+        
+        let found = false;
+
+        for (let obj of this.data) {
+            for (let prop in obj) {
+                if (obj[prop] === id) {
+                    found = true;
+                    this.data = this.data.filter(user => user.id != id)                }
+            }
+        }
+        if (found === true) {
+            return true;
+        } else {
+            return false;    
+        }
+        
+    }
+
+    // this method will be overriden in the sub classes
+    validate(obj) {
+        return false;
+    }
+}
+
+// Do not worry about the below for now; It is included so that we can test your code
+// We will cover module exports in later parts of this course
+module.exports = DataModel;
